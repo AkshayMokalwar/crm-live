7d328e6012f63a67fbbb58ca0d064e0152754b2a
Akshay  Mokalwar <67907470+AkshayMokalwar@users.noreply.github.com>
2021-01-11 23:16:26 +0530

    Delete admin.py

--- testapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from testapp.models import student
-# Register your models here.
-class studentAdmin(admin.ModelAdmin):
-	list_diplay=['studentname','studentage','username','password','studentaddress']
-admin.site.register(student,studentAdmin)
